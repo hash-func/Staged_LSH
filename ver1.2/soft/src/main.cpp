756d117d057e51109e13d329c1e00f5745ad523d
hash-func <hashfunc945@gmail.com>
2022-04-12 19:06:56 +0900

    vitisコード作成（仮）

--- ver1.2/soft/src/main.cpp
+++ ver1.2/soft/src/main.cpp
@@ -51,20 +51,91 @@ int main(int argc, char** argv)
     hash_table_pointer = (unsigned int*) aligned_alloc(division_num, sizeof(unsigned int)*division_num);
     unsigned int* hash_table;                   // ハッシュテーブル
     hash_table = (unsigned int*) aligned_alloc(full_table_size ,sizeof(unsigned int)*full_table_size);
-    unsigned int query[ONEMUSIC_SUBNUM];        // 検索クエリの一時格納配列 
+    //unsigned int* query;                        // 検索クエリの一時格納配列
+    //query = (unsigned int*) aligned_alloc(ONEMUSIC_SUBNUM, sizeof(unsigned int)*ONEMUSIC_SUBNUM); 
     //unsigned char* bit_element;                 // Hash関数bit取得位置(96までなのでchar)
     //bit_element = (unsigned char*) calloc(K_HASHBIT*L_HASHNUM, sizeof(unsigned char));
     unsigned int bit_element[K_HASHBIT*L_HASHNUM] = {41, 1, 32, 12, 74, 90, 81, 9, 64, 43, 46, 75, 90, 35};
     unsigned int* flame_addr;                   // 各フレームの先頭アドレス(一時処理使用)
     flame_addr = (unsigned int*) malloc(sizeof(unsigned int)*MUSIC_NUM*FLAME_IN_MUSIC);
     /* --必要配列(ヒープ領域)-- */
+    
 
-    /* 変数宣言 */
-    int judge_temp = -1;                        // 結果判定用(一時格納)
+/****************************************************************************************************/
 
+    /* 楽曲DBの作成(ele_func.cpp) */
+    fp_db_generator(
+        FP_DB,                  // FPデータベース
+        MUSIC_NUM,              // 楽曲数
+        ONEMUSIC_SUBNUM         // 1曲あたりのsubFP数
+    );
+#ifdef DEBUG
+    printf("DB作成完了\n");
+#endif
+#ifdef ELEBITGET
+    /* Hash関数bit取得位置決定配列生成(ele_func.cpp) */
+    bit_element_get(bit_element,                // bit取得位置
+                    K_HASHBIT,                  // ハッシュ関数gのbit数
+                    L_HASHNUM,                  // ハッシュ関数の数
+                    SUB_FP_SIZE,                // subFPサイズ(bit)
+                    SUBNUM_IN_FLAME             // 1フレームあたりのsubFP数
+                    );
+#endif
+#ifdef DEBUG_sub
+    printf("bit取得位置格納完了\n");
+#endif
+    /* 各フレームへの番地を配列に格納(ele_func.cpp) */
+    flame_addr_get( 
+        FP_DB,                      // FPデータベース
+        flame_addr,                 // 各フレームの先頭アドレス
+        MUSIC_NUM,                  // 楽曲数
+        ONEMUSIC_SUBNUM,            // 1曲あたりのsubFP数
+        FLAME_IN_MUSIC              // 1曲あたりのflame数
+        );
+#ifdef DEBUG
+    printf("フレーム先頭位置格納完了\n");
+#endif
+/****************************************************************************************************/
+    /* Hashテーブルへの要素の格納 */
+    hash_table_stock(
+        FP_DB,                     // FPデータベース
+        hash_table,                // ハッシュテーブル
+        hash_table_pointer,        // ハッシュテーブル末尾位置指定
+        flame_addr,                // 各フレーム先頭アドレス
+        bit_element,               // bit取得位置
+        division_num,              // ハッシュテーブル分割数
+        full_table_size,           // 全ハッシュテーブルサイズ
+        MUSIC_NUM,                 // 楽曲数
+        FLAME_IN_MUSIC,            // 1曲あたりのflame数
+        K_HASHBIT,                 // ハッシュ関数のbit数
+        L_HASHNUM                  // ハッシュ関数の数
+        );
+#ifdef DEBUG_sub
+    printf("Hashテーブルへの要素格納完了\n");
+#endif
+#ifdef DEBUG_sub
+    if (hash_table_pointer[division_num-1] == full_table_size-1)
+    {
+        printf ("hash_table_pointer異常なし\n");
+    }
+    else
+    {
+        printf ("hash_table_pointer : %u\n", hash_table_pointer[division_num-1]);
+        printf ("full_table_size : %u\n", full_table_size-1);
+        printf ("hash_table_pointer異常\n");
+        exit(1);
+    }
+#endif
+    free(flame_addr);
 
+    /* 結果格納変数 */
+    unsigned int music_index = 0;       // 検索楽曲識別子(0~)
+    int judge_temp = -1;                // 結果判定用(一時格納)
+    unsigned int seikai = 0;            // 正解数
+    unsigned int huseikai = 0;          // 偽陽性数（結果は出たが誤り）
+    unsigned int not_find = 0;          // 未発見(負数)
 
-/***********************************************************************************************************/
+/****************************************************************************************************/
 // OpenCLホストコードエリア
     //--------------------------------------------------------------
     // Step:1 OpenCL環境の初期化
@@ -105,8 +176,8 @@ int main(int argc, char** argv)
     // バッファの作成とメモリの確保
     // バッファの作成
     // ホスト側のバッファメモリをユーザー空間のポインタにマップ
-    cl::Buffer query_buf(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-    sizeof(int)*ONEMUSIC_SUBNUM, query, &err);
+    cl::Buffer query_buf(context, CL_MEM_READ_ONLY,
+    sizeof(int)*ONEMUSIC_SUBNUM, NULL, &err);
     cl::Buffer FP_DB_buf(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, 
     sizeof(unsigned int)*MUSIC_NUM*ONEMUSIC_SUBNUM, FP_DB, &err);
     cl::Buffer hash_table_buf(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, 
@@ -116,45 +187,75 @@ int main(int argc, char** argv)
     cl::Buffer jadge_temp_buf(context, CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY, sizeof(int),
     judge_temp, &err);
 
+    krnl_table_serch.setArg(0, query_buf);
+
+    unsigned int* query = (unsigned int*)q.enqueueMapBuffer(
+        query_buf, CL_TRUE, CL_MAP_WRITE, 0, sizeof(unsigned int)*ONEMUSIC_SUBNUM);
+
     //--------------------------------------------------------------
     // Step:3 カーネルの実行
     //--------------------------------------------------------------
 
-    // バッファをカーネル引数にマッピングし、特定のデバイスメモリバンクに割り当てる
-    krnl_table_serch.setArg(0, query_buf);
-    krnl_table_serch.setArg(1, FP_DB_buf);
-    krnl_table_serch.setArg(2, hash_table_buf);
-    krnl_table_serch.setArg(3, hash_table_pointer_buf);
-    krnl_table_serch.setArg(4, judge_temp_buf);
+    /* 指定回数検索実行 */
+    for (int i=0; i<QUERY_NUM; i++)
+    {
+#ifdef DEBUG_sub
+        printf("%d 回目\n",i+1);
+#endif
+        /* 楽曲識別子生成 */
+        music_index = rnd1() % MUSIC_NUM;
 
-    // 入力のデバイスメモリへの転送
-    // ホストからデバイスのグローバルメモリに転送 読み込みだけ
-    q.enqueueMigrateMemObjects({
-        query_buf,
-        FP_DB_buf,
-        hash_table_buf,
-        hash_table_pointer_buf
-    },0 /*0はホストからの意味*/);
+        /* index楽曲格納 + 歪みのあるクエリの作成(ele_func.cpp) */
+        distortion_query_create(
+            FP_DB,                              // FPデータベース
+            query,                              // クエリ格納配列
+            music_index,                        // 楽曲識別子
+            DISTORTION,                         // 楽曲歪み率
+            ONEMUSIC_SUBNUM                     // 1曲あたりのsubFP数
+        );
 
-    // カーネルの実行
-    q.enqueueTask(krnl_table_serch);
+        /* 検索処理（FPGA） */
+        // バッファをカーネル引数にマッピングし、特定のデバイスメモリバンクに割り当てる
+        krnl_table_serch.setArg(0, query_buf);
+        krnl_table_serch.setArg(1, FP_DB_buf);
+        krnl_table_serch.setArg(2, hash_table_buf);
+        krnl_table_serch.setArg(3, hash_table_pointer_buf);
+        krnl_table_serch.setArg(4, judge_temp_buf);
 
-    // 出力のホストメモリへの転送をスケジュール
-    // 結果の取得
-    q.enqueueMigrateMemObjects({judge_temp_buf}, CL_MIGRATE_MEM_OBJECT_HOST);
+        // 入力のデバイスメモリへの転送
+        // ホストからデバイスのグローバルメモリに転送 読み込みだけ
+        q.enqueueMigrateMemObjects({
+            query_buf,
+            FP_DB_buf,
+            hash_table_buf,
+            hash_table_pointer_buf
+        },0 /*0はホストからの意味*/);
 
-    // スケジュールされた全ての動作終了まで待ち
-    q.finish();
+        // カーネルの実行
+        q.enqueueTask(krnl_table_serch);
 
-    //--------------------------------------------------------------
-    // Step:4 結果の確認と割り当てられたリソースの解放
-    //--------------------------------------------------------------
+        // 出力のホストメモリへの転送をスケジュール
+        // 結果の取得
+        q.enqueueMigrateMemObjects({judge_temp_buf}, CL_MIGRATE_MEM_OBJECT_HOST);
 
+        // スケジュールされた全ての動作終了まで待ち
+        q.finish();
 
+        /* 結果の集計 */
+        if (judge_temp < 0) not_find++;
+        else
+        {
+            if (music_index == judge_temp)  seikai++;
+            else                            huseikai++;
+        }
+    }
 
 /****************************************************************************************************/
     /* 結果の表示 */
-    printf("\n");
+    printf ("\n");
+    printf ("正解率 : %lf %\n", ((double)seikai/QUERY_NUM)*100);
+    printf ("不正解率 : %lf %\n", ((double)huseikai/QUERY_NUM)*100);
+    printf ("未発見 : %lf %\n", ((double)not_find/QUERY_NUM)*100);
 /****************************************************************************************************/
 
     /* 後処理後終了 */
@@ -162,13 +263,11 @@ int main(int argc, char** argv)
     free(FP_DB);
     free(hash_table_pointer);
     free(hash_table);
-    free(flame_addr);
 
 #ifdef DEBUG
     printf("処理終了\n");
 #endif
     return 0;
-
 }
 /*-- main --*/
 
